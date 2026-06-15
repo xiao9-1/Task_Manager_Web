847ec3d697395c189014c5add6ec2764a16b21e2
Kirill <matveeffew@gmail.com>
2026-06-15 12:24:50 +0300

    Роутинг для перехода по страницам

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { TasksComponent } from './tasks/tasks.component';
+import { UserTasksComponent } from './userTasks/userTasks.component';
+import { AdminTasksComponent } from './adminTasks/adminTasks.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'tasks', component: TasksComponent }
+  { path: 'tasks', component: UserTasksComponent },
+  {path: 'admin/tasks', component: AdminTasksComponent}
 ];
