0a81fbf526996ed03c7595f8643e17611e4bbcf3
Kirill <matveeffew@gmail.com>
2026-06-17 16:21:27 +0300

    removed /admin/tasks/ route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { UserTasksComponent } from './userTasks/userTasks.component';
-import { AdminTasksComponent } from './adminTasks/adminTasks.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'tasks', component: UserTasksComponent },
-  {path: 'admin/tasks', component: AdminTasksComponent}
+  { path: 'tasks', component: TasksComponent }
 ];
