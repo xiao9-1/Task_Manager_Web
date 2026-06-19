0910f30cc490dfb3ce8023af050dc6f902afd7d2
Kirill <matveeffew@gmail.com>
2026-06-19 12:49:55 +0300

    redirect to task if unknown page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { TasksComponent } from './tasks/tasks.component';
+import { LoginComponent } from './pages/login/login.component';
+import {TasksComponent} from './pages/tasks/components/tasks.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'tasks', component: TasksComponent }
+  { path: 'tasks', component: TasksComponent },
+  { path: '**', redirectTo: 'tasks' }
 ];
